e3a14886921202c74d4906946d1d9b4713a4b76f
boyanroyachki <106555994+boyanroyachki@users.noreply.github.com>
2024-02-20 17:58:29 +0200

    Added a Common class library that will hold some constant values. Added some summaries.

---
+++ Common.Triditech/GeneralApplicationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Triditech
+{
+	/// <summary>
+	/// Class that holds some constant values that will be used troughout the whole application.
+	/// </summary>
+	public static class GeneralApplicationConstants
+	{
+		public const string CreatedOn = "20/02/2024";
+	}
+}
